59cef5af3807a12bd236865697e53ac8ecd12d2d
Andy Sonnenburg <andy22286@gmail.com>
2014-07-14 07:00:23 -0400

    working marking

--- src/foul/mark.c
+++ src/foul/mark.c
@@ -1,41 +1,34 @@
 #include "mark.h"
 
-#include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
-#include <stdio.h>
 
 void set_marked(foul_obj_t *obj) {
 	obj->vtable = (foul_obj_vtable_t *) (((uintptr_t) obj->vtable) | 1);
 }
 
 void foul_mark(foul_obj_t *obj) {
-	for (foul_obj_t *parent = NULL, *child = obj;;) {
-		puts("current");
-		set_marked(child);
-		printf("offset: %u\n", (unsigned int) child->offset);
-		if (foul_begin(child) + child->offset != foul_end(child)) {
-			puts("down");
-			foul_obj_t **i = foul_begin(child) + child->offset;
-			++child->offset;
-			printf("%p\n", i);
+	for (foul_obj_t *parent = NULL;;) {
+		set_marked(obj);
+		foul_obj_t **i = foul_begin(obj) + obj->offset;
+		if (i != foul_end(obj)) {
 			foul_obj_t *tmp = *i;
-			printf("%p\n", tmp);
 			if (!foul_is_marked(tmp)) {
 				*i = parent;
-				parent = child;
-				child = tmp;
+				parent = obj;
+				obj = tmp;
+			} else {
+				++obj->offset;
 			}
 		} else {
-			puts("up");
 			if (!parent) {
-				return;
+				break;
 			}
 			foul_obj_t *tmp = parent;
-			foul_obj_t **i = foul_begin(tmp) + tmp->offset;
+			i = foul_begin(tmp) + tmp->offset;
 			parent = *i;
-			*i = child;
-			child = tmp;
+			*i = obj;
+			obj = tmp;
 		}
 	}
 }
